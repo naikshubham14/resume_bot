251dff06e8fd815cff15b00fbaf8f12ee5edd70e
Shubham Naik <shubhamnaik321@gmail.com>
2025-01-28 12:30:21 +0530

    LLM Upgrade

--- helper.py
+++ helper.py
@@ -153,7 +153,7 @@ def llm_call(prompt):
     }
 
     model = genai.GenerativeModel(
-    model_name="gemini-2.0-flash-exp",
+    model_name="gemini-2.0-flash-thinking-exp-01-21",
     generation_config=generation_config,
     )
 
